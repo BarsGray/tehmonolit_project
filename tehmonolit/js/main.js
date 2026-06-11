af306589918479247d8f1f8b53a2483bd2a1d636
Daniil <m2140sr@gmail.com>
2026-06-11 13:30:47 +0300

    work_5

---
+++ tehmonolit/js/main.js
@@ -0,0 +1,25 @@
+window.addEventListener('scroll', function () {
+  let scroll_scr = window.scrollY;
+  if (scroll_scr > 0) {
+    document.querySelector('header').classList.add('active');
+  } else {
+    document.querySelector('header').classList.remove('active');
+  }
+});
+
+const menuButton = document.querySelector('.burger_menu_btn');
+const svgMenuButton = document.querySelector('.burger_menu_btn .ham');
+const headerMenu = document.querySelector('.menu_wrap');
+const overlay = document.querySelector('.overlay');
+
+function openMenu() {
+  document.querySelector('body').classList.toggle('scroll-nane');
+
+  menuButton.classList.toggle('burger_menu_btn--active');
+  svgMenuButton.classList.toggle('active');
+  headerMenu.classList.toggle('active');
+  overlay.classList.toggle('visible');
+}
+
+menuButton.addEventListener('click', openMenu);
+overlay.addEventListener('click', openMenu);
